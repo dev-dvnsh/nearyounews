3a2bca14eeab847edf86929caa4f2de790b053be
Devansh Bhardwaj <dbhardwaj123411@gmail.com>
2026-01-14 22:59:42 +0530

    Location model added

--- backend/controllers/location.controller.js
+++ backend/controllers/location.controller.js
@@ -1,24 +1,33 @@
 const express = require("express");
+const Location = require("../models/Location.js");
+const receiveLocation = async (req, res) => {
+  try {
+    // reading data from req body
+    const { latitude, longitude } = req.body;
 
-const receiveLocation = (req, res) => {
-  // reading data from req body
-  const { latitude, longitude } = req.body;
+    if (!latitude || !longitude) {
+      return res.status(400).json({
+        success: false,
+        message: "Latitude and longitude are required",
+      });
+    }
 
-  if (!latitude || !longitude) {
-    return res.status(400).json({
-      success: false,
-      message: "Latitude and longitude are required",
-    });
-  }
-
-  return res.status(200).json({
-    sucess: true,
-    message: "Location received successfully",
-    data: {
+    const location = new Location({
       latitude,
       longitude,
-    },
-  });
+    });
+
+    await location.save();
+    return res.status(201).json({
+      sucess: true,
+      message: "Location saved successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
 };
 
 module.exports = {
---
+++ backend/models/Location.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const locationScheme = new mongoose.Schema(
+  {
+    latitude: {
+      type: Number,
+      required: true,
+    },
+    longitude: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Create model from the schema
+const Location = mongoose.model("Location", locationScheme);
+
+model.exports = Location;
